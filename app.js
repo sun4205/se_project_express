17c63beee7deefd3648d09c9ff57fd8bad9ef33d
sun4205 <hanna4205@gmail.com>
2024-11-28 13:23:23 -0500

    set up project

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+const { PORT = 3001 } = process.env;
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/wtwr_db")
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch(console.error);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
