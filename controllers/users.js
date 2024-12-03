81d753f742c743f7d9866843f6cf8bc9888809b0
sun4205 <hanna4205@gmail.com>
2024-12-02 21:41:37 -0500

    users fix

--- controllers/users.js
+++ controllers/users.js
@@ -1,55 +1,71 @@
+const mongoose = require("mongoose");
 const User = require("../models/user");
-const { BAD_REQUEST, NOT_FOUND, INTERNAL_SERVER_ERROR } = require("../utils/errors");
-
+const {
+  BAD_REQUEST,
+  NOT_FOUND,
+  INTERNAL_SERVER_ERROR,
+} = require("../utils/errors");
 
 const getUsers = (req, res) => {
   User.find({})
     .then((users) => res.status(200).send(users))
     .catch((err) => {
       console.error(err);
-      return res.status(INTERNAL_SERVER_ERROR).send({ message: "An error has occurred on the server." });
+      return res
+        .status(INTERNAL_SERVER_ERROR)
+        .send({ message: "An error has occurred on the server." });
     });
 };
 
-
 const getUser = (req, res) => {
   const { userId } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(userId)) {
+    return res.status(BAD_REQUEST).send({ message: "Invalid user ID." });
+  }
+
   User.findById(userId)
-  .orFail(() => {
-    const error = new Error("User not found.");
-    error.statusCode = NOT_FOUND;
-    throw error;
-  })
+    .orFail(() => {
+      const error = new Error("User not found.");
+      error.statusCode = NOT_FOUND;
+      throw error;
+    })
     .then((user) => {
-      if (!user) {
-        return res.status(NOT_FOUND).send({ message: "User not found." });
-      }
       res.status(200).send(user);
     })
     .catch((err) => {
       console.error(err);
+
       if (err.name === "CastError") {
         return res.status(BAD_REQUEST).send({ message: "Invalid user ID." });
       }
-      return res.status(INTERNAL_SERVER_ERROR).send({ message: "An error has occurred on the server." });
+
+      return res
+        .status(INTERNAL_SERVER_ERROR)
+        .send({ message: "An error has occurred on the server." });
     });
 };
-
 
 const createUser = (req, res) => {
   console.log("Request body:", req.body);
   const { name, avatar } = req.body;
   if (!name || !avatar) {
-    return res.status(400).send({ message: "Both 'name' and 'avatar' are required." });
+    return res
+      .status(400)
+      .send({ message: "Both 'name' and 'avatar' are required." });
   }
   User.create({ name, avatar })
     .then((user) => res.status(201).send(user))
     .catch((err) => {
       console.error(err);
       if (err.name === "ValidationError") {
-        return res.status(BAD_REQUEST).send({ message: "Both 'name' and 'avatar' are required." });
+        return res
+          .status(BAD_REQUEST)
+          .send({ message: "Both 'name' and 'avatar' are required." });
       }
-      return res.status(INTERNAL_SERVER_ERROR).send({ message: "An error has occurred on the server." });
+      return res
+        .status(INTERNAL_SERVER_ERROR)
+        .send({ message: "An error has occurred on the server." });
     });
 };
 
