77687152c3d07db6660518218cda362b626cad9c
sun4205 <hanna4205@gmail.com>
2024-12-04 20:58:23 -0500

    add app express

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const itemRouter = require("./clothingItems");
 const { NOT_FOUND } = require("../utils/errors");
 
 const router = express.Router();
+const app = express();
 
 router.use("/users", userRouter);
 router.use("/items", itemRouter);
