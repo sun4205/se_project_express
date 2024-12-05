0ecb145dddb3ae29b1c0128f4f2df384f479e692
sun4205 <hanna4205@gmail.com>
2024-12-04 20:50:49 -0500

    app.use moving

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const userRouter = require("./users");
 const itemRouter = require("./clothingItems");
-const { NOT_FOUND } = require("./utils/errors");
+const { NOT_FOUND } = require("../utils/errors");
 
 const router = express.Router();
 
