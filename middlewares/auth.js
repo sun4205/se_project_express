3a90eb05fa3b6d5f93f92b6fe5a18d9e182fcee0
sun4205 <hanna4205@gmail.com>
2024-12-17 11:15:23 -0500

    create middleware, config

---
+++ middlewares/auth.js
@@ -0,0 +1,27 @@
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../utils/config");
+const { UNAUTHORIZED } = require("../utils/errors");
+
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith("Bearer ")) {
+    return res.status(UNAUTHORIZED).send({ message: "Authorization required" });
+  }
+
+  const token = authorization.replace("Bearer ", "");
+  let payload;
+  
+  try {
+    payload = jwt.verify(token,JWT_SECRET);
+  } catch (err) {
+    return res
+      .status(401)
+      .send({ message: 'Invalid token' });
+  }
+
+  req.user = payload; 
+
+  next(); 
+
+};
--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,22 @@
 const express = require("express");
+const auth = require("../middlewares/auth");
 const userRouter = require("./users");
 const itemRouter = require("./clothingItems");
-const { login, createUser } = require("../controllers/users"); 
+const { login, createUser } = require("../controllers/users");
 const { NOT_FOUND } = require("../utils/errors");
 
 const router = express.Router();
 
+router.post("/signin", login);
+router.post("/signup", createUser);
+router.get("/items", itemRouter);
+
+router.use(auth);
 
 router.use("/users", userRouter);
-router.use("/items", itemRouter);
-
-router.post('/signin', login);
-router.post('/signup', createUser);
 
 router.use((req, res) => {
   res.status(NOT_FOUND).send({ message: "Requested resource not found." });
 });
 
 module.exports = router;
-
-
