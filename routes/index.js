6ee573b04b1fb0ec62c3639a9d3424d0f8e2b036
sun4205 <hanna4205@gmail.com>
2025-01-11 20:03:04 -0500

    route fix

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,11 @@ const { NOT_FOUND } = require("../utils/errors");
 
 const router = express.Router();
 
-router.post("/signin", login);
-router.post("/signup", createUser);
+ router.post("/signin", login);
+ router.post("/signup", createUser);
+
 
 router.use("/items", itemRouter);
-
 router.use("/users", userRouter);
 
 router.use((req, res) => {
