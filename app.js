9d9da953e7630a05b71d23e6e23cf4396877c898
sun4205 <hanna4205@gmail.com>
2025-01-30 14:13:56 -0500

    add dotenv

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const helmet = require("helmet");
 const mongoose = require("mongoose");
 const cors = require("cors");
+require('dotenv').config();
 const { errors } = require("celebrate");
 
 const mainRouter = require("./routes/index");
--- utils/config.js
+++ utils/config.js
@@ -1,3 +1,3 @@
-const JWT_SECRET = "super-strong-secret";
+const {JWT_SECRET = "super-strong-secret"} = process.env;
 
 module.exports = { JWT_SECRET };
