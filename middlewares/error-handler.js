7f1f9d06eac4e50c29319469c7c0079d32928d5b
sun4205 <hanna4205@gmail.com>
2025-01-31 11:26:07 -0500

    fix

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -1,15 +1,10 @@
-const errorHandler = (err, req, res,next) => {
-    console.error(err);
-    // if an error has no status, set it to 500
-    const { statusCode = 500, message } = err;
-    res
-      .status(statusCode)
-      .send({
-        // check the status and display a message based on it
-        message: statusCode === 500
-          ? 'An error occurred on the server'
-          : message
-      });
-  };
+const errorHandler = (err, req, res, next) => {
+  console.error(err);
 
-  module.exports = errorHandler;
+  const { statusCode = 500, message } = err;
+  res.status(statusCode).send({
+    message: statusCode === 500 ? "An error occurred on the server" : message,
+  });
+};
+
+module.exports = errorHandler;
