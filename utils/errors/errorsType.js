f0391f2dca4e41fd96ce67bf3a259562ee3ef76e
sun4205 <hanna4205@gmail.com>
2025-02-11 17:56:36 -0500

    errorType uncomment

--- utils/errors/errorsType.js
+++ utils/errors/errorsType.js
@@ -1,15 +1,15 @@
-// const BadRequestError = require("./BadRequestError");
-// const ConflictError = require("./ConflictError");
-// const ForbiddenError = require("./ForbiddenError");
-// const InternalServerError = require("./InternalServerError");
-// const NotFoundError = require("./NotFoundError");
-// const UnauthorizedError = require("./UnauthorizedError");
+const BadRequestError = require("./BadRequestError");
+const ConflictError = require("./ConflictError");
+const ForbiddenError = require("./ForbiddenError");
+const InternalServerError = require("./InternalServerError");
+const NotFoundError = require("./NotFoundError");
+const UnauthorizedError = require("./UnauthorizedError");
 
-// module.exports = {
-//     BadRequestError,
-//     ConflictError,
-//     ForbiddenError,
-//     InternalServerError,
-//     NotFoundError,
-//     UnauthorizedError,
-// }
+module.exports = {
+    BadRequestError,
+    ConflictError,
+    ForbiddenError,
+    InternalServerError,
+    NotFoundError,
+    UnauthorizedError,
+}
