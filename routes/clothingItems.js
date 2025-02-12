ba608b8211f9c892896d56df3ed5fc778812267b
sun4205 <hanna4205@gmail.com>
2025-02-11 20:06:58 -0500

    add celebrate

--- routes/clothingItems.js
+++ routes/clothingItems.js
@@ -6,13 +6,14 @@ const {
   likeItem,
   dislikeItem,
 } = require("../controllers/clothingItems");
+const {validateClothingItem,validatedId} = require("../middlewares/validation");
 const auth = require("../middlewares/auth");
 
 router.get("/", getItems);
-router.post("/", auth, createItem);
-router.delete("/:itemId", auth, deleteItem);
+router.post("/", auth, validateClothingItem, createItem);
+router.delete("/:itemId", auth, validatedId, deleteItem);
 
-router.put("/:itemId/likes", auth, likeItem);
-router.delete("/:itemId/likes", auth, dislikeItem);
+router.put("/:itemId/likes", auth, validatedId, likeItem);
+router.delete("/:itemId/likes", auth, validatedId, dislikeItem);
 
 module.exports = router;
